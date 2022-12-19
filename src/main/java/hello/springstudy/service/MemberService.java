802e3ccd804f14a0fd68a4acb18057bf6c019335
semyeongJ <94214574+semyeongJ@users.noreply.github.com>
2022-12-19 23:45:18 +0900

    feat: transform into DI

--- src/main/java/hello/springstudy/service/MemberService.java
+++ src/main/java/hello/springstudy/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemoryMemberRepository memberRepository) {     // DI (Dependency Injection)
+        this.memberRepository = memberRepository;
+    }
 
     /* 회원가입 */
     public Long join(Member member){
@@ -19,13 +23,9 @@ public class MemberService {
     }
 
     private void validateDuplicateMember(Member member) {
-        memberRepository.findByName(member.getName())
-                .ifPresent(m -> {
-                    try {
-                        throw new IllegalAccessException("이미 존재하는 회원입니다.");
-                    } catch (IllegalAccessException e) {
-                        e.printStackTrace();
-                    }
+        memberRepository.findByName(member.getName()) //ctrl+alt+v
+                .ifPresent(m -> { //result가 null이 아니면 즉, 값이 있으면 동작
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
 
