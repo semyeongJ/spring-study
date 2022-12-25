2f5330517f7034385e4f54fd3b813ac09d2c9d38
semyeongJ <94214574+semyeongJ@users.noreply.github.com>
2022-12-25 17:58:49 +0900

    회원 웹 기능 - 등록

---
+++ src/main/java/hello/springstudy/controller/MemberForm.java
@@ -0,0 +1,15 @@
+package hello.springstudy.controller;
+
+public class MemberForm {
+    private String name;
+
+
+    // 단축키: command + N
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
