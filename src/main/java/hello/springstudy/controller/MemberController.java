0fdbfed075bed9acc21cd4ab75535f734b3ce519
semyeongJ <94214574+semyeongJ@users.noreply.github.com>
2022-12-25 18:10:59 +0900

    회원 웹 기능 - 조회

--- src/main/java/hello/springstudy/controller/MemberController.java
+++ src/main/java/hello/springstudy/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.springstudy.domain.Member;
 import hello.springstudy.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,5 +34,13 @@ public class MemberController {
 
         // redirect 완료되면 홈화면으로 리다이렉트
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
